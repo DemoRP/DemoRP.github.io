4240adbd1a1efebf0ad4df45a6ce78a79e8516d2
DemoRP <84363239+DemoRP@users.noreply.github.com>
2021-05-19 13:12:01 +0700

    add http server

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const port = process.env.PORT || 1883
 server.listen(port, function () {
   console.log('server started and listening on port ', port)
 })
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(process.env.PORT||80);
 
