c26df46db1bca9d73fd3858e9a6799a69a3cfc6b
DemoRP <84363239+DemoRP@users.noreply.github.com>
2021-05-19 13:30:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const aedes = require("aedes")();
+const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
 const port = process.env.PORT || 80;
+
 ws.createServer({ server: httpServer }, aedes.handle);
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(port);
+
+httpServer.listen(port, () => {
+  console.log("websocket server listening on port ", port);
+});
