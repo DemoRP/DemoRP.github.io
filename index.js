31455be53c400b63ee535474a88a03a7ca088d75
DemoRP <84363239+DemoRP@users.noreply.github.com>
2021-05-19 13:48:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const port = process.env.PORT || 1883;
 server.listen(port, function () {
   console.log('server started and listening on port ', port)
 })
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(process.env.PORT || 80);
