dda4f810d3e6b998ddede35490c93a780c725eb2
DemoRP <84363239+DemoRP@users.noreply.github.com>
2021-05-19 13:49:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const aedes = require('aedes')()
-const server = require('net').createServer(aedes.handle)
-const port = process.env.PORT || 1883;
+// const aedes = require('aedes')()
+// const server = require('net').createServer(aedes.handle)
+// const port = process.env.PORT || 1883;
 
-server.listen(port, function () {
-  console.log('server started and listening on port ', port)
-})
+// server.listen(port, function () {
+//   console.log('server started and listening on port ', port)
+// })
 var http = require('http');
 
 http.createServer(function (req, res) {
